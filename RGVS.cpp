4aa37ed2df9ff6cec7cbfd262e9d7f614471591e
Skylar Moore <nickgray0@gmail.com>
2016-01-18 07:27:54 -0800

    Update RGVS.cpp

--- RGVS.cpp
+++ RGVS.cpp
@@ -1,7 +1,7 @@
 #include "shared.h"
 
 VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {
-	configFunc("com.vapoursynth.removegrainvs", "rgsf", "RemoveGrain VapourSynth Port", VAPOURSYNTH_API_VERSION, 1, plugin);
+	configFunc("com.vapoursynth.removegrainsf", "rgsf", "RemoveGrain Single Precision", VAPOURSYNTH_API_VERSION, 1, plugin);
 	registerFunc("RemoveGrain", "clip:clip;mode:int[];", removeGrainCreate, nullptr, plugin);
 	registerFunc("Repair", "clip:clip;repairclip:clip;mode:int[];", repairCreate, nullptr, plugin);
 	registerFunc("VerticalCleaner", "clip:clip;mode:int[];", verticalCleanerCreate, nullptr, plugin);
